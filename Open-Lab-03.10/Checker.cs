2db6f896661079989d233c1d66bcd33b2b0fa324
Michal Holena <113421705+MichalProfi@users.noreply.github.com>
2022-10-13 08:52:56 +0200

    check how many letters in string

--- Open-Lab-03.10/Checker.cs
+++ Open-Lab-03.10/Checker.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-
+using System.Linq;
 namespace Open_Lab_03._10
 {
     public class Checker
     {
         public int GetNumberOfCharsInString(char letter, string str)
         {
-            throw new NotImplementedException();
+            return str.Count(a => a == letter);
         }
     }
 }
